84924f9afb744a2e9011ffdacfec20544d739f19
Quang N Huynh <quangnhuynh@kms-technology.com>
2025-07-04 22:51:43 +0700

    Completed System Notes view

--- src/FileCabinet/SuiteScripts/EmployeeLogin/sl/sl_login_drilldown.js
+++ src/FileCabinet/SuiteScripts/EmployeeLogin/sl/sl_login_drilldown.js
@@ -2,11 +2,35 @@
  * @NApiVersion 2.1
  * @NScriptType Suitelet
  */
-define(['N/ui/serverWidget'],
+/* global log */
+define([
+    'N/ui/serverWidget',
+    'N/file',
+    'N/render',
+
+    // Libraries
+    '/SuiteScripts/EmployeeLogin/lib/External/lodash.min.js',
+
+    // Custom Libraries
+    '/SuiteScripts/EmployeeLogin/lib/Custom/QHSuiteQL.js',
+],
     /**
- * @param{serverWidget} serverWidget
- */
-    (serverWidget) => {
+     * @param{serverWidget} serverWidget
+     */
+    (
+        serverWidget,
+        file,
+        render,
+
+        _,
+
+        QHSuiteQL
+    ) => {
+
+        const VIEW_TEMPLATE = 'SuiteScripts/EmployeeLogin/template/systemnote_view.html';
+        const SQL_Last6Months_SystemNoteByUserRole = `/SuiteScripts/EmployeeLogin/sql/SystemNoteByUserAndRole.sql`;
+        const PAGE_SIZE = 20; // Default page size
+
         /**
          * Defines the Suitelet script trigger point.
          * @param {Object} scriptContext
@@ -15,9 +39,99 @@ define(['N/ui/serverWidget'],
          * @since 2015.2
          */
         const onRequest = (scriptContext) => {
+            const { request, response } = scriptContext;
 
+            
+            if (request.method == 'GET') {
+                const { user, role, page } = request.parameters;
+                try {
+                    // Find User name and Role name
+                    const form = serverWidget.createForm({
+                        title: 'System Notes for'
+                    });
+
+                    let fld_html_view = form.addField({
+                        id: 'custpage_html_template',
+                        label: 'View',
+                        type: serverWidget.FieldType.INLINEHTML
+                    });
+
+                    let tmplFile = file.load({
+                        id: VIEW_TEMPLATE
+                    });
+
+                    if (tmplFile) {
+                        let data = getData({ user, role, pageSize: PAGE_SIZE, pageIndex: page });
+                        data.user? form.title += ` ${data.user}` : form.title += ' Unknown User';
+                        data.role? form.title += ` (Role: ${data.role})` : form.title += ' (Unknown Role)';
+
+                        let htmlContent = tmplFile.getContents();
+
+                        let renderer = render.create();
+                        renderer.templateContent = htmlContent;
+                        renderer.addCustomDataSource({
+                            format: render.DataSource.OBJECT,
+                            alias: "data",
+                            data: { str: JSON.stringify(data) }
+                        });
+                        fld_html_view.defaultValue = renderer.renderAsString();
+                    }
+
+                    response.writePage(form);
+                } catch (error) {
+                    log.error('Error rendering GET response', error);
+                    response.write(JSON.stringify({ error: 'An error occurred while processing your request.' }));
+                }
+            } else {
+                log.error('Unsupported request method', `Received ${request.method}`);
+                response.write('Unsupported request method');
+
+            }
         }
 
-        return {onRequest}
+        const getData = (options) => {
+            const { user, role, pageSize, pageIndex } = options;
+            log.debug('getData - options', options);
+
+            let data = { data: [], monthlabel: ['Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown'] };
+
+            if (!user || !role) {
+                log.error('getData - Missing parameters', 'User and Role are required');
+                throw 'User and Role are required';
+            }
+
+            let querydata = QHSuiteQL.getResults({
+                sqlFile: SQL_Last6Months_SystemNoteByUserRole,
+                absolutePath: true,
+                modules: {
+                    file: file
+                },
+                parameters: {
+                    user: user ? ` AND s.name = ${user} ` : '',
+                    role: role ? ` AND s.role = ${role} ` : '',
+                },
+                pageSize: pageSize || 20,
+                pageIndex: Math.max((pageIndex || 0) - 1, 0)
+            });
+            log.debug('querydata', querydata);
+
+            data = {
+                monthlabel: querydata.Records?.length ? [
+                    querydata.Records[0].month5_label,
+                    querydata.Records[0].month4_label,
+                    querydata.Records[0].month3_label,
+                    querydata.Records[0].month2_label,
+                    querydata.Records[0].month1_label,
+                    querydata.Records[0].month0_label
+                ] : [],
+                user: querydata.Records?.length ? querydata.Records[0].user : '',
+                role: querydata.Records?.length ? querydata.Records[0].role : '',
+                ...querydata,
+            };
+            log.debug('getData', data);
+            return data;
+        }
+
+        return { onRequest }
 
     });
---
+++ src/FileCabinet/SuiteScripts/EmployeeLogin/sql/SystemNoteByUserAndRole.sql
@@ -0,0 +1,46 @@
+SELECT 
+    BUILTIN.DF(s.name) as user,
+    BUILTIN.DF(s.role) as role,
+    COALESCE(BUILTIN.DF(s.context), 'Unknown') as context,
+    s.recordTypeId,
+    COALESCE(BUILTIN.DF(s.recordTypeId), 'Unknown') as recordTypeName,
+    s.recordId,
+    COALESCE(s.record, 'Unknown') as record,
+    SUM(CASE WHEN to_char(s.date, 'YYYY-MM') = to_char(ADD_MONTHS(CURRENT_DATE(), 0), 'YYYY-MM') THEN 1 ELSE 0 END) +
+	SUM(CASE WHEN to_char(s.date, 'YYYY-MM') = to_char(ADD_MONTHS(CURRENT_DATE(), -1), 'YYYY-MM') THEN 1 ELSE 0 END) +
+	SUM(CASE WHEN to_char(s.date, 'YYYY-MM') = to_char(ADD_MONTHS(CURRENT_DATE(), -2), 'YYYY-MM') THEN 1 ELSE 0 END) +
+	SUM(CASE WHEN to_char(s.date, 'YYYY-MM') = to_char(ADD_MONTHS(CURRENT_DATE(), -3), 'YYYY-MM') THEN 1 ELSE 0 END) +
+	SUM(CASE WHEN to_char(s.date, 'YYYY-MM') = to_char(ADD_MONTHS(CURRENT_DATE(), -4), 'YYYY-MM') THEN 1 ELSE 0 END) +
+	SUM(CASE WHEN to_char(s.date, 'YYYY-MM') = to_char(ADD_MONTHS(CURRENT_DATE(), -5), 'YYYY-MM') THEN 1 ELSE 0 END) AS total,
+	SUM(CASE WHEN to_char(s.date, 'YYYY-MM') = to_char(ADD_MONTHS(CURRENT_DATE(), 0), 'YYYY-MM') THEN 1 ELSE 0 END) AS month0,
+	SUM(CASE WHEN to_char(s.date, 'YYYY-MM') = to_char(ADD_MONTHS(CURRENT_DATE(), -1), 'YYYY-MM') THEN 1 ELSE 0 END) AS month1,
+	SUM(CASE WHEN to_char(s.date, 'YYYY-MM') = to_char(ADD_MONTHS(CURRENT_DATE(), -2), 'YYYY-MM') THEN 1 ELSE 0 END) AS month2,
+	SUM(CASE WHEN to_char(s.date, 'YYYY-MM') = to_char(ADD_MONTHS(CURRENT_DATE(), -3), 'YYYY-MM') THEN 1 ELSE 0 END) AS month3,
+	SUM(CASE WHEN to_char(s.date, 'YYYY-MM') = to_char(ADD_MONTHS(CURRENT_DATE(), -4), 'YYYY-MM') THEN 1 ELSE 0 END) AS month4,
+	SUM(CASE WHEN to_char(s.date, 'YYYY-MM') = to_char(ADD_MONTHS(CURRENT_DATE(), -5), 'YYYY-MM') THEN 1 ELSE 0 END) AS month5,
+	to_char(ADD_MONTHS(CURRENT_DATE(), 0), 'Mon YYYY') as month0_label,
+	to_char(ADD_MONTHS(CURRENT_DATE(), -1), 'Mon YYYY') as month1_label,
+	to_char(ADD_MONTHS(CURRENT_DATE(), -2), 'Mon YYYY') as month2_label,
+	to_char(ADD_MONTHS(CURRENT_DATE(), -3), 'Mon YYYY') as month3_label,
+	to_char(ADD_MONTHS(CURRENT_DATE(), -4), 'Mon YYYY') as month4_label,
+	to_char(ADD_MONTHS(CURRENT_DATE(), -5), 'Mon YYYY') as month5_label
+FROM (
+    SELECT * FROM SystemNote ORDER BY date DESC
+) s
+WHERE s.date BETWEEN TRUNC(ADD_MONTHS(CURRENT_DATE(), -5),'mm') AND LAST_DAY(CURRENT_DATE())
+    @{user}
+    @{role}
+GROUP BY
+    BUILTIN.DF(s.name),
+    BUILTIN.DF(s.role),
+    BUILTIN.DF(s.context),
+    s.recordTypeId,
+    BUILTIN.DF(s.recordTypeId),
+    s.recordId,
+    s.record,
+    to_char(ADD_MONTHS(CURRENT_DATE(), 0), 'Mon YYYY'),
+    to_char(ADD_MONTHS(CURRENT_DATE(), -1), 'Mon YYYY'),
+    to_char(ADD_MONTHS(CURRENT_DATE(), -2), 'Mon YYYY'),
+    to_char(ADD_MONTHS(CURRENT_DATE(), -3), 'Mon YYYY'),
+    to_char(ADD_MONTHS(CURRENT_DATE(), -4), 'Mon YYYY'),
+    to_char(ADD_MONTHS(CURRENT_DATE(), -5), 'Mon YYYY')
